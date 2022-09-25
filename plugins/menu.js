7446e83e569bdcde820db181ed8873a8a899b7e0
OffChampion <112922324+OffChampion@users.noreply.github.com>
2022-09-25 13:52:03 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -250,7 +250,7 @@ let tek = `*${ucapan()} ${conn.getName(m.sender)}*
 `
 const listMessage = {
   text: tek,
-  footer: '© by 𝕻𝖚𝖙 𝕯𝖊𝖛',
+  footer: '© by 𝖈𝖍𝖆𝖒𝖕𝖎𝖔𝖓',
   mentions: await conn.parseMention(tek),
   title: `${htki} *LIST MENU* ${htka}`,
   buttonText: `CLICK HERE ⎙`,
